25200321ebe0b8e06498b2ae17e43eb5e0691732
Chrysolite Nocon and Frances Canon <pair+cnocon+fcanon@spins.com>
2025-11-13 09:34:04 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./styles/App.css";
+import { fetchSmileyEmojis } from "./services/emojiApi";
+
+function App() {
+  const [decoyEmoji, setDecoyEmoji] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchSmileyEmojis()
+      .then((data) => setDecoyEmoji(data))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const tileEmojis = () => {
+    const tiles = [];
+    for (let i = 0; i < 64; i++) {
+      for (let j = 0; j < 64; j++) {
+        tiles.push(
+          <div className="tile" key={`${i}, ${j}`}>
+            <img src={decoyEmoji[0].image} />
+          </div>
+        );
+      }
+    }
+    return tiles;
+  };
+
+  console.log(decoyEmoji[0]);
+  return (
+    <div className="game-body">
+      <div className="header">
+        <h2>emoji hunt!</h2>
+        <div className="timer">00:00</div>
+        <div className="score">
+          <p>score: 0</p>
+          <p>highscore: 0</p>
+        </div>
+      </div>
+
+      <div className="tiles">
+        {tileEmojis()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
